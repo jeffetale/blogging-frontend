3bae893ad69269b4cfe5e3fb54947ba0c224ff78
jeffetale <jeff.etale@student.moringaschool.com>
2025-01-09 17:31:06 +0300

    image rendering in about page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -11,7 +11,7 @@ export default function ProfilePage() {
 
   useEffect(() => {
     if (!loggedIn) {
-      router.push("/login");
+      router.push("/signin");
     }
   }, [loggedIn, router]);
 
